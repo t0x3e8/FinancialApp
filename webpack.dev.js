4b60cf4267d3990cbb436eb0ecccca33d8bf4cf8
Jarek <t0x3e8@gmail.com>
2022-03-26 12:08:19 +0100

    Webpack updated to ES module script

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,29 +1,27 @@
-const path = require("path");
-const { VueLoaderPlugin } = require("vue-loader");
+import { resolve } from "path";
+import { VueLoaderPlugin } from "vue-loader";
 
-module.exports = {
-  mode: "development",
-  entry: "./src/main.js",
-  output: {
-    path: path.resolve(__dirname, "public"),
-    publicPath: "/public/javascripts",
-    filename: "app.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/u,
-        loader: "vue-loader",
-      },
-      {
-        test: /\.js$/u,
-        exclude: "/node_modules/",
-        loader: "babel-loader",
-        options: {
-          presets: [["@babel/preset-env", { targets: "defaults" }]],
-        },
-      },
-    ],
-  },
-  plugins: [new VueLoaderPlugin()],
+export const mode = "development";
+export const entry = "./src/main.js";
+export const output = {
+  path: resolve(__dirname, "public"),
+  publicPath: "/public/javascripts",
+  filename: "app.js",
 };
+export const module = {
+  rules: [
+    {
+      test: /\.vue$/u,
+      loader: "vue-loader",
+    },
+    {
+      test: /\.js$/u,
+      exclude: "/node_modules/",
+      loader: "babel-loader",
+      options: {
+        presets: [["@babel/preset-env", { targets: "defaults" }]],
+      },
+    },
+  ],
+};
+export const plugins = [new VueLoaderPlugin()];
