30e06cd015c542a4ce3a0c12b028781e705a7187
Jarek <t0x3e8@gmail.com>
2022-03-26 11:58:37 +0100

    socialInsurance calculation

--- src/logic/employmentContractCalculator.js
+++ src/logic/employmentContractCalculator.js
@@ -1,16 +1,23 @@
 import _ from "underscore";
 
+const SocialInsuranceAnnualLimit = 177660;
+
 class EmploymentContractCalculator {
   constructor(contractData) {
     if (_.isNull(contractData) || _.isUndefined(contractData)) {
       contractData = {};
     }
 
-    this.salaryInMonths =
-      _.isNull(contractData.salaryInMonths) || _.isUndefined(contractData.salaryInMonths)
-        ? []
-        : contractData.salaryInMonths;
+    this.salaryInMonths = _.isNull(contractData.salaryInMonths) || _.isUndefined(contractData.salaryInMonths) ? [] : contractData.salaryInMonths;
+
     this.accSalaryinMonths = [];
+    this.socialInsurance = {
+      retirementInsurance: [],
+      disabilityInsurance: [],
+      sicknessInsurance: [],
+      healthInsurance: [],
+      total: [],
+    };
 
     this.calculate();
   }
@@ -24,8 +31,38 @@ class EmploymentContractCalculator {
     });
   }
 
+  calculateSociatInsurance() {
+    _.each(this.salaryInMonths, (salary, index) => {
+      if (this.accSalaryinMonths[index] < SocialInsuranceAnnualLimit) {
+        this.socialInsurance.retirementInsurance[index] = Math.round(salary * 0.0976);
+        this.socialInsurance.disabilityInsurance[index] = Math.round(salary * 0.015);
+      } else {
+        var deltaSalary = salary - this.accSalaryinMonths[index] - SocialInsuranceAnnualLimit;
+        if (deltaSalary < 0) {
+          this.socialInsurance.retirementInsurance[index] = 0;
+          this.socialInsurance.disabilityInsurance[index] = 0;
+        } else {
+          this.socialInsurance.retirementInsurance[index] = Math.round(deltaSalary * 0.0976);
+          this.socialInsurance.disabilityInsurance[index] = Math.round(deltaSalary * 0.015);
+        }
+      }
+
+      this.socialInsurance.sicknessInsurance[index] = Math.round(salary * 0.0245);
+      this.socialInsurance.healthInsurance[index] = Math.floor(
+        (salary - this.socialInsurance.retirementInsurance[index] - this.socialInsurance.disabilityInsurance[index] - this.socialInsurance.sicknessInsurance[index]) * 0.09
+      );
+
+      this.socialInsurance.total[index] =
+        this.socialInsurance.retirementInsurance[index] +
+        this.socialInsurance.disabilityInsurance[index] +
+        this.socialInsurance.sicknessInsurance[index] +
+        this.socialInsurance.healthInsurance[index];
+    });
+  }
+
   calculate() {
     this.calculateAccumulatedSalaries();
+    this.calculateSociatInsurance();
   }
 }
 
--- test/employmentContractCalculator.test.js
+++ test/employmentContractCalculator.test.js
@@ -2,23 +2,33 @@ import EmploymentContractCalculator from "../src/logic/EmploymentContractCalcula
 
 var eccData = {
   sameSalaryInMonths: {
-    salaryInMonths: [100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100],
+    salaryInMonths: [5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000, 5000],
   },
   sameSalaryInMonthsResults: {
-    accSalaryinMonths: [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200],
-  }
+    accSalaryinMonths: [5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000, 45000, 50000, 55000, 60000],
+    retirementInsurance: [488, 488, 488, 488, 488, 488, 488, 488, 488, 488, 488, 488],
+    disabilityInsurance: [75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 75, 75],
+    sicknessInsurance: [123, 123, 123, 123, 123, 123, 123, 123, 123, 123, 123, 123],
+    healthInsurance: [388, 388, 388, 388, 388, 388, 388, 388, 388, 388, 388, 388],
+    total: [1074, 1074, 1074, 1074, 1074, 1074, 1074, 1074, 1074, 1074, 1074, 1074],
+  },
 };
 
-test("If no input is specified, set the default value", () => {
+test("If no input is specified, set the default values.", () => {
   const ecc = new EmploymentContractCalculator();
 
   expect(ecc.salaryInMonths).toEqual(expect.arrayContaining([]));
   expect(ecc.accSalaryinMonths).toHaveLength(0);
 });
 
-test("If 'SalaryInMonths' input is specified, set the value", () => {
+test("If 'SalaryInMonths' input is specified, calculate 'accSalaryinMonths', 'socialInsurance'.", () => {
   const ecc = new EmploymentContractCalculator(eccData.sameSalaryInMonths);
 
   expect(ecc.salaryInMonths).toHaveLength(12);
   expect(ecc.accSalaryinMonths).toEqual(expect.arrayContaining(eccData.sameSalaryInMonthsResults.accSalaryinMonths));
+  expect(ecc.socialInsurance.retirementInsurance).toEqual(expect.arrayContaining(eccData.sameSalaryInMonthsResults.retirementInsurance));
+  expect(ecc.socialInsurance.disabilityInsurance).toEqual(expect.arrayContaining(eccData.sameSalaryInMonthsResults.disabilityInsurance));
+  expect(ecc.socialInsurance.sicknessInsurance).toEqual(expect.arrayContaining(eccData.sameSalaryInMonthsResults.sicknessInsurance));
+  expect(ecc.socialInsurance.healthInsurance).toEqual(expect.arrayContaining(eccData.sameSalaryInMonthsResults.healthInsurance));
+  expect(ecc.socialInsurance.total).toEqual(expect.arrayContaining(eccData.sameSalaryInMonthsResults.total));
 });
